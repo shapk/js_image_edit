0257522a856b8337df99d6153246022414d844aa
xuefen <xuefennh@yahoo.cn>
2013-04-13 20:09:31 +0800

    创建
    创建

---
+++ imageEdit.js
@@ -0,0 +1,12 @@
+(function($){
+	$.fn.imageEdit = function(){
+		var l1 = $(".l1");
+		var l2 = $(".l2");
+		var l3 = $(".l3");
+		
+		$(this).click(function(e){
+			var x = e.pageX;
+			var y = e.pageY;
+		});
+	};
+})(jQuery);
